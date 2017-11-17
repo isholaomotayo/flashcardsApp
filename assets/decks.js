b68d28d9bc565b62b037dc182ca78ec7ba666406
Omotayo ISHOLA <isholaomotayo@yahoo.com>
2017-11-17 23:56:33 +0100

    Initial Commit App built to a considerable level with working functionality FUNCT: :100: Add Deck ✅ All decks view ✅ Single decks view ✅ Navigation ✅ Navigation TODO: :alarm_clock: Add card :alarm_clock: Quiz :alarm_clock: Animations :alarm_clock: ALocal Push Notifications

---
+++ assets/decks.js
@@ -0,0 +1,24 @@
+export default  decks = {
+  Mythology: {
+    title: 'Mythology',
+    questions: [
+      {
+        question: 'A 1990 ad for jeans helped put which early 70s Steve Miller Band song to the top of the charts?',
+        answer: 'The Joker'
+      },
+      {
+        question: 'A car with the international registration letter E comes from where?',
+        answer: 'Spain'
+      }
+    ]
+  },
+  Technology: {
+    title: 'Technology',
+    questions: [
+      {
+        question: 'What is a closure?',
+        answer: 'The combination of a function and the lexical environment within which that function was declared.'
+      }
+    ]
+  }
+}
